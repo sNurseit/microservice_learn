5b7cb293cce7688b8b8c51512d8d465ab51f33f8
sNurseit <nurseit.taimas@gmail.com>
2024-03-19 15:47:55 +0500

    update client service fix properties

--- eureka-client/src/main/java/kz/nurs/micro_demo/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/kz/nurs/micro_demo/eurekaclient/EurekaClientApplication.java
@@ -2,8 +2,10 @@ package kz.nurs.micro_demo.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {

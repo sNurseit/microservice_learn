fd5c24f00358a72811e4d8f9c099ef6496503f33
sNurseit <nurseit.taimas@gmail.com>
2024-04-03 18:56:16 +0500

    update api gateway property Add main class

---
+++ api-gateway/src/test/java/kz/nurs/micro_demo/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nurs.micro_demo.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

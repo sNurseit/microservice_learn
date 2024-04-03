89ed21eb5d6dd9a518e2ffeee77e94e661680931
sNurseit <nurseit.taimas@gmail.com>
2024-04-03 18:54:21 +0500

    update client service fix properties

--- eureka-client/src/main/java/kz/nurs/micro_demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/kz/nurs/micro_demo/eurekaclient/controller/TestController.java
@@ -1,12 +1,14 @@
 package kz.nurs.micro_demo.eurekaclient.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/main")
     public String test(){
         return "text";
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
 rootProject.name = "micro_demo"
 
 includeBuild( "eureka-server")
+includeBuild("eureka-client")
+includeBuild("api-gateway")
+includeBuild("client-two")
